ef89d68e897801f3addd12cf1048a03d60a479e8
PARK YESUNG <cjsrnr0504@naver.com>
2024-04-12 16:13:23 +0900

    24.04.12 학원

---
+++ src/com/javaex/ex09/OnetoTenEx.java
@@ -0,0 +1,17 @@
+package com.javaex.ex09;
+
+public class OnetoTenEx {
+
+	public static void main(String[] args) {
+
+		for(int i = 1; i <=10; i++) {
+			for(int a = i ;a <= i; a++) {
+				System.out.print(a);
+				
+			}
+			System.out.print(i);
+		}
+
+	}
+
+}
---
+++ src/com/javaex/ex10/NumFiveEx.java
@@ -0,0 +1,24 @@
+package com.javaex.ex10;
+
+import java.util.Scanner;
+
+public class NumFiveEx {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("숫자를 입력하세요");
+		System.out.print("숫자: ");
+		System.out.print("숫자: ");
+		System.out.print("숫자: ");
+		System.out.print("숫자: ");
+		System.out.print("숫자: ");
+		
+		int num1 = sc.nextInt();
+		for(int i = 1; i < 5; i++) {
+			
+		}
+
+	}
+
+}
